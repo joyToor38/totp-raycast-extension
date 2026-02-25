ce267f6a0b5c2a63ce9b5077993dcbccd121a38e
Joy Toor <joytoor@Joys-MacBook-Pro.local>
2026-02-25 23:06:34 +0530

    update: commands for list and create

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,45 @@
-import { List } from "@raycast/api";
+import { Action, ActionPanel, Form, showToast, Toast, useNavigation } from "@raycast/api";
+import { useForm, FormValidation } from "@raycast/utils";
+import { generate } from "otplib";
+
+interface SignUpFormValues {
+  name: string;
+}
 
 export default function Command() {
+  const { handleSubmit, itemProps } = useForm<SignUpFormValues>({
+    async onSubmit(values) {
+      const {pop} = useNavigation()
+      const secret = await generate({secret: values.name})
+      console.log(secret)
+      showToast({
+        style: Toast.Style.Success,
+        title: "Yay!",
+        message: `${values.name} account created`,
+      });
+      pop()
+    },
+    validation: {
+      name: FormValidation.Required,
+      // password: (value) => {
+      //   if (value && value.length < 8) {
+      //     return "Password must be at least 8 symbols";
+      //   } else if (!value) {
+      //     return "The item is required";
+      //   }
+      // },
+    },
+  });
+
   return (
-    <List>
-        <p>hello</p>
-    </List>
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Submit" onSubmit={handleSubmit} />
+        </ActionPanel>
+      }
+    >
+      <Form.TextField title="Full Name" placeholder="Tim Cook" {...itemProps.name} />
+    </Form>
   );
 }
